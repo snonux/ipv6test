0da1e78a3e88e9a29ce8b9e2ec01c5f181cf94fe
Paul Buetow <paul@buetow.org>
2026-01-29 09:26:20 +0200

    Make HTML W3C compliant by adding proper DOCTYPE, charset, and semantic elements

--- docker-image/index.pl
+++ docker-image/index.pl
@@ -8,23 +8,25 @@ use warnings;
 my $server_name = $ENV{SERVER_NAME} // 'ipv6test.f3s.buetow.org';
 
 print <<END;
-Content-type: text/html
+Content-type: text/html; charset=UTF-8
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+<meta charset="UTF-8">
 <title>The Ultimate IPv6 Test Site</title>
 </head>
 <body>
 
-Congratulations, you have connected to a server that will display your method of connection, either IPv6 (preferred) or IPv4 (old and crusty). Well IPv6 is already ~15 years old either but not as old as IPv4 ;)
-<br /><br />
-Nevertheless, please choose your destiny:
+<p>Congratulations, you have connected to a server that will display your method of connection, either IPv6 (preferred) or IPv4 (old and crusty). Well IPv6 is already ~15 years old either but not as old as IPv4 ;)</p>
+
+<p>Nevertheless, please choose your destiny:</p>
 <ul>
 	<li><a href="https://ipv6test.f3s.buetow.org">ipv6test.f3s.buetow.org</a> for IPv6 & IPv4 Test (Dual Stack)</li>
 	<li><a href="https://ipv4.ipv6test.f3s.buetow.org">ipv4.ipv6test.f3s.buetow.org</a> for IPv4 Only Test</li>
 	<li><a href="https://ipv6.ipv6test.f3s.buetow.org">ipv6.ipv6test.f3s.buetow.org</a> for IPv6 Only Test</li>
 </ul>
-If your browser times-out when trying to connect to this server then you do not have an IPv6 or IPv4 path (depends on which test you are running) to the server. If your browser returns an error that the host cannot be found then the DNS servers you are using are unable to resolve the AAAA or A DNS record (depends on which test you are running again) for the server. If your browser is able to connect to the "IPv6 Only Test", yet using the "IPv6 & IPv4 Test" returns a page stating you are using IPv4, then your browser and/or IP stack in your machine are preferring IPv4 over IPv6. It also might be that your operating system supports IPv6 but your web-browser doesn't.
+<p>If your browser times-out when trying to connect to this server then you do not have an IPv6 or IPv4 path (depends on which test you are running) to the server. If your browser returns an error that the host cannot be found then the DNS servers you are using are unable to resolve the AAAA or A DNS record (depends on which test you are running again) for the server. If your browser is able to connect to the "IPv6 Only Test", yet using the "IPv6 & IPv4 Test" returns a page stating you are using IPv4, then your browser and/or IP stack in your machine are preferring IPv4 over IPv6. It also might be that your operating system supports IPv6 but your web-browser doesn't.</p>
 END
 
 if ($server_name =~ /^ipv6test\.f3s\.buetow\.org$/) {
@@ -63,26 +65,26 @@ print <<END;
 Client address: $ENV{REMOTE_ADDR}
 Server address: $ENV{SERVER_ADDR}
 
-<b>Client address reverse DNS lookup:</b>
+<strong>Client address reverse DNS lookup:</strong>
 $remote
 
-<b>Server address reverse DNS lookup:</b>
+<strong>Server address reverse DNS lookup:</strong>
 $server
 
-<b>Server hostname DNS lookup:</b>
+<strong>Server hostname DNS lookup:</strong>
 $server0
 
-<b>Advanced client address reverse DNS lookup:</b>
+<strong>Advanced client address reverse DNS lookup:</strong>
 $digremote
 
-<b>Advanced server address reverse DNS lookup:</b>
+<strong>Advanced server address reverse DNS lookup:</strong>
 $digserver
 
-<b>Advanced server hostname DNS lookup:</b>
+<strong>Advanced server hostname DNS lookup:</strong>
 $digserver0
 </pre>
 <hr />
-Thanks for visiting, please recommend this test to your friends and colleagues. Any comments go to <a href="https://paul.buetow.org">Paul Buetow</a>.
+<p>Thanks for visiting, please recommend this test to your friends and colleagues. Any comments go to <a href="https://paul.buetow.org">Paul Buetow</a>.</p>
 </body>
 </html>
 END
